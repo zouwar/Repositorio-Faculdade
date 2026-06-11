8ff5ca4d7318d746739fc75a21dab4e6e95e4b23
JVzNDEV <joaovictoralves01052005@gmail.com>
2026-06-10 21:28:19 -0300

    Adicionado bolinhas para escolher o slide na pág principal e ajuste do dark mode na pág de cadastro

--- projeto/js/darkmode.js
+++ projeto/js/darkmode.js
@@ -1,20 +1,15 @@
-const button = document.getElementById("toggleDark");
-
+const button = document.getElementById("modoEscuro");
 // carregar preferência
 if (localStorage.getItem("dark-mode") === "true") {
     document.body.classList.add("dark-mode");
     button.textContent = "☀️";
 }
-
 // clique
 button.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
-
     const isDark = document.body.classList.contains("dark-mode");
-
     // trocar ícone
     button.textContent = isDark ? "☀️" : "🌙";
-
     // salvar
     localStorage.setItem("dark-mode", isDark);
 });
--- projeto/js/login.js
+++ projeto/js/login.js
@@ -1,45 +1,28 @@
 let form = document.getElementById("formLogin");
-
 form.addEventListener("submit", function(e){
-
     e.preventDefault();
-
     let email = document.getElementById("email").value;
     let senha = document.getElementById("senha").value;
-
     let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
     console.log(usuarios);
     let loginCorreto = false;
-
     for(let i = 0; i < usuarios.length; i++){
-
         if(
             usuarios[i].email == email &&
             usuarios[i].senha == senha
         ){
-
             loginCorreto = true;
-
             localStorage.setItem(
                 "usuarioLogado",
                 JSON.stringify(usuarios[i])
             );
-
         }
-
     }
-
     if(loginCorreto){
-
         alert("Login realizado!");
-
         window.location.href = "../index.html";
-
     }else{
-
         document.getElementById("mensagem").innerHTML =
         "Email ou senha incorretos";
-
     }
-
 });
